b2fd1ab5d6c2e22bacac736fd5f36a2571052a8b
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-03-29 20:02:53 +0200

    update

---
+++ ratp-stations/stations_extérieures.sql
@@ -0,0 +1,9 @@
+
+
+ALTER TABLE ratp_stations
+    ADD Outside BOOLEAN
+    DEFAULT FALSE NOT NULL;
+
+UPDATE ratp_stations SET outside = TRUE WHERE name = 'Bastille'
+UPDATE ratp_stations SET outside = TRUE WHERE name = 'Esplanade de la Défense'
+-- ajouter reste
