93e72404d26c624c8a4c5d92ad587db946e1dbd2
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-04-03 22:37:23 +0200

    add metros & RERs

--- ratp-stations/lignes_metro_annexe.sql
+++ ratp-stations/lignes_metro_annexe.sql
@@ -1,9 +1,6 @@
-ALTER TABLE "ratp_stations"
-	RENAME description to address;
-
 /* Source: https://fr.wikipedia.org/wiki/M%C3%A9tro_de_Paris#Lignes_en_service */ 
 
-CREATE TABLE metro_annexe (
+CREATE TABLE lignes_metro_annexe (
 	ligne VARCHAR NOT NULL,
 	parcours VARCHAR NOT NULL,
 	mise_en_service INT NOT NULL,
@@ -17,7 +14,7 @@ CREATE TABLE metro_annexe (
 	millions_voyageurs_2017 FLOAT NOT NULL
 );
 
-INSERT INTO metro_annexe (ligne, parcours, mise_en_service, dernier_prolongement, longueur_km_surface, nombre_stations, materiel, voitures_par_rame, nombre_rames_pointe, nombre_total_rames, millions_voyageurs_2017) 
+INSERT INTO lignes_metro_annexe (ligne, parcours, mise_en_service, dernier_prolongement, longueur_km_surface, nombre_stations, materiel, voitures_par_rame, nombre_rames_pointe, nombre_total_rames, millions_voyageurs_2017) 
 	VALUES
 		('1', 'La Défense - Château de Vincennes', 1900, 1992, 16.6, 25, MP 05, 6, 45, 56, 181.2),
 		('2', 'Porte Dauphine - Nation', 1900, 1903, 12.3, 25, MF 01,	5, 37, 46, 105.2),
--- ratp-stations/ratp_stations.sql
+++ ratp-stations/ratp_stations.sql
@@ -9,9 +9,31 @@ CREATE TABLE ratp_stations (
 
 ALTER TABLE ratp_stations
     ADD train_outside BOOLEAN DEFAULT FALSE NOT NULL,
-    ADD transport_type VARCHAR DEFAULT 'bus' NOT NULL;
+    ADD transport_type VARCHAR DEFAULT 'bus' NOT NULL,
+    ADD line VARCHAR NULL;
     
+-- Define records with non-UPPERCASE names as metros
 
-UPDATE ratp_stations SET train_outside = TRUE WHERE name = 'Bastille';
-UPDATE ratp_stations SET train_outside = TRUE WHERE name = 'Esplanade de la Défense';
--- ajouter reste
+UPDATE ratp_stations
+    SET transport_type = 'metro'
+    WHERE 'name' <> UPPER('name');
+
+-- Define RER stations with unique names
+
+UPDATE ratp_stations SET transport_type = 'rer' WHERE "name" IN ('Achères Grand Cormier','Achères-Ville','Aéroport Charles de Gaulle 1','Aéroport Charles de Gaulle 2 TGV','Antony','Arcueil-Cachan','Auber','Aulnay-sous-Bois','Bagneux','Boissy-Saint-Léger','Bourg-la-Reine','Bry-sur-Marne','Bures-sur-Yvette','Bussy-Saint-Georges','Cergy-Le-Haut','Cergy-Préfecture','Cergy-Saint-Christophe','Champigny','Châtelet-Les Halles','Chatou-Croissy','Cité Universitaire','Conflans-Fin d''Oise','Courcelle-sur-Yvette','Denfert-Rochereau','Drancy','Fontaine-Michalon','Fontenay-aux-Roses','Fontenay-sous-Bois','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères','Achères');
+
+-- Define RER stations which have metro stations with the same name
+
+-- Charles de Gaulle - Étoile
+UPDATE ratp_stations SET transport_type = 'rer' WHERE 'coordinates' = '48.8739040986,2.29503190105';
+-- Gare de Lyon
+UPDATE ratp_stations SET transport_type = 'rer' WHERE 'coordinates' = '48.844472156,2.3739114213';
+-- Gare du Nord
+UPDATE ratp_stations SET transport_type = 'rer' WHERE 'coordinates' = '48.8808392058,2.35657701535';
+
+
+-- Define all exterior train stations
+
+-- Add lines for each tain station
+
+-- Remove identical db records
