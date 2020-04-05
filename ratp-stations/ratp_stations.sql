4cf2090723b2ce3016f6c889401e9f9ace63e6c1
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-04-06 01:41:55 +0200

    update

--- ratp-stations/ratp_stations.sql
+++ ratp-stations/ratp_stations.sql
@@ -40,51 +40,56 @@ UPDATE ratp_stations SET train_outside = TRUE WHERE "name" IN ('Bir-Hakeim (Gren
 UPDATE ratp_stations SET train_outside = TRUE WHERE "coordinates" IN ('48.8533891123,2.36916460221','48.8839317699,2.3493557086','48.8841892942,2.36703044197','48.8827685467,2.36994579899','48.8437198614,2.36391621716','48.8490189283,2.29776216071');
 
 -- Add lines for train stations which have only 1 line
-UPDATE ratp_stations SET line = '1' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '2' WHERE "name" IN ('La Chapelle','');
-UPDATE ratp_stations SET line = '3' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '3bis' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '4' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '5' WHERE "name" IN ('Quai de la Rapée','');
-UPDATE ratp_stations SET line = '6' WHERE "name" IN ('Bir-Hakeim (Grenelle)','Cambronne','Chevaleret','Corvisart','Dupleix','Glacière','Quai de la Gare','Sèvres-Lecourbe','Bel-Air','Passy','Saint-Jacques');
-UPDATE ratp_stations SET line = '7' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '7bis' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '8' WHERE "name" IN ('Créteil-L''Echat (Hôpital Henri Mondor)','Créteil-Préfecture (Hôtel de Ville)','Créteil-Université','Pointe du Lac');
-UPDATE ratp_stations SET line = '9' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '10' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '11' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '12' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = '13' WHERE "name" IN ('Châtillon Montrouge','Malakoff-Rue Etienne Dolet');
-UPDATE ratp_stations SET line = '14' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = 'A' WHERE "name" IN ('','');
-UPDATE ratp_stations SET line = 'B' WHERE "name" IN ('','');
+UPDATE ratp_stations SET line = '1' WHERE "name" IN ('Argentine','Bérault','','','','','','','');
+UPDATE ratp_stations SET line = '2' WHERE "name" IN ('La Chapelle','Alexandre-Dumas','Anvers','Avron','Blanche','','','','','','','','');
+UPDATE ratp_stations SET line = '3' WHERE "name" IN ('Anatole-France','Bourse','','','','','','','','','','');
+UPDATE ratp_stations SET line = '3bis' WHERE "name" IN ('','','','','','','','','','','');
+UPDATE ratp_stations SET line = '4' WHERE "name" IN ('Alésia','','','','','','','','');
+UPDATE ratp_stations SET line = '5' WHERE "name" IN ('Quai de la Rapée','Bobigny-Pablo-Picasso','Bobigny-Pantin (Raymond Queneau)','Bréguet-Sabin','Campo-Formio','','','','','');
+UPDATE ratp_stations SET line = '6' WHERE "name" IN ('Bir-Hakeim (Grenelle)','Cambronne','Chevaleret','Corvisart','Dupleix','Glacière','Quai de la Gare','Sèvres-Lecourbe','Bel-Air','Passy','Saint-Jacques','Boissière','','','','','','','','','','','','');
+UPDATE ratp_stations SET line = '7' WHERE "name" IN ('Aubervilliers Pantin (4 Chemins)','Cadet','Censier-Daubenton','','','','','','','','');
+UPDATE ratp_stations SET line = '7bis' WHERE "name" IN ('Bolivar','Botzaris','Buttes-Chaumont','','','','','');
+UPDATE ratp_stations SET line = '8' WHERE "name" IN ('Créteil-L''Echat (Hôpital Henri Mondor)','Créteil-Préfecture (Hôtel de Ville)','Créteil-Université','Pointe du Lac','Balard','Boucicaut','Charenton-Ecoles','','','','','','','');
+UPDATE ratp_stations SET line = '9' WHERE "name" IN ('Alma-Marceau','Billancourt','Buzenval','','','','','');
+UPDATE ratp_stations SET line = '10' WHERE "name" IN ('Avenue Emile-Zola','Boulogne-Jean-Jaurès','Boulogne Pont de Saint-Cloud','Cardinal-Lemoine','Chardon-Lagache','','','','');
+UPDATE ratp_stations SET line = '11' WHERE "name" IN ('','','','','','','','','');
+UPDATE ratp_stations SET line = '12' WHERE "name" IN ('Abbesses','Assemblée Nationale','','','','','','','','','','','','','','','','','');
+UPDATE ratp_stations SET line = '13' WHERE "name" IN ('Châtillon Montrouge','Malakoff-Rue Etienne Dolet','Basilique de Saint-Denis','Brochant','Carrefour-Pleyel','','','','','','','');
+UPDATE ratp_stations SET line = '14' WHERE "name" IN ('Bibliothèque-François Mitterrand','','','','','','','');
+UPDATE ratp_stations SET line = 'A' WHERE "name" IN ('','','','','','','','');
+UPDATE ratp_stations SET line = 'B' WHERE "name" IN ('','','','','','','','');
 
 -- Add lines for train stations which have several lines by their coordinates
--- Bastille, 
-UPDATE ratp_stations SET line = '1' WHERE "coordinates" IN ('48.8533891123,2.36916460221','');
--- Barbès-Rochechouart, Stalingrad, Jaurès
-UPDATE ratp_stations SET line = '2' WHERE "coordinates" IN ('48.8839317699,2.3493557086','48.8841892942,2.36703044197','48.8827685467,2.36994579899');
-UPDATE ratp_stations SET line = '3' WHERE "coordinates" IN ('','');
+-- Bastille, Champs-Élysées Clémenceau
+UPDATE ratp_stations SET line = '1' WHERE "coordinates" IN ('48.8533891123,2.36916460221','48.8673665705,2.31419106339');
+-- Barbès-Rochechouart, Stalingrad, Jaurès, Belleville
+UPDATE ratp_stations SET line = '2' WHERE "coordinates" IN ('48.8839317699,2.3493557086','48.8841892942,2.36703044197','48.8827685467,2.36994579899','48.8722873206,2.37673767732');
+-- Arts et Métiers, 
+UPDATE ratp_stations SET line = '3' WHERE "coordinates" IN ('48.8655518636,2.35610815512','');
 UPDATE ratp_stations SET line = '3bis' WHERE "coordinates" IN ('','');
 -- Barbès-Rochechouart, 
 UPDATE ratp_stations SET line = '4' WHERE "coordinates" IN ('48.8836801093,2.34953280365','');
 -- Bastille, Stalingrad, Jaurès, Gare d'Austerlitz
 UPDATE ratp_stations SET line = '5' WHERE "coordinates" IN ('48.8529756747,2.36921882444','48.8841532631,2.36735747747','48.8823280249,2.37047696877','48.8437198614,2.36391621716');
--- La Motte-Picquet-Grenelle
-UPDATE ratp_stations SET line = '6' WHERE "coordinates" IN ('48.8490189283,2.29776216071','');
+-- La Motte-Picquet-Grenelle, Bercy
+UPDATE ratp_stations SET line = '6' WHERE "coordinates" IN ('48.8490189283,2.29776216071','48.840542783,2.37940946312');
 -- Stalingrad
 UPDATE ratp_stations SET line = '7' WHERE "coordinates" IN ('48.8840719864,2.36887006278','');
 -- Jaurès
 UPDATE ratp_stations SET line = '7bis' WHERE "coordinates" IN ('48.8828762162,2.37058633617','');
--- Bastille, La Motte-Picquet-Grenelle
-UPDATE ratp_stations SET line = '8' WHERE "coordinates" IN ('48.853739623,2.36916481786','48.8488212667,2.29795296403');
-UPDATE ratp_stations SET line = '9' WHERE "coordinates" IN ('','');
+-- Bastille, La Motte-Picquet-Grenelle, Bonne-Nouvelle
+UPDATE ratp_stations SET line = '8' WHERE "coordinates" IN ('48.853739623,2.36916481786','48.8488212667,2.29795296403','48.8705767536,2.34849429371');
+-- Bonne-Nouvelle
+UPDATE ratp_stations SET line = '9' WHERE "coordinates" IN ('48.8704958323,2.34886211744','');
 -- Gare d'Austerlitz, La Motte-Picquet-Grenelle
 UPDATE ratp_stations SET line = '10' WHERE "coordinates" IN ('48.8434142241,2.36418839287','48.848803206,2.29769423466');
-UPDATE ratp_stations SET line = '11' WHERE "coordinates" IN ('','');
+-- Arts et métiers, Belleville
+UPDATE ratp_stations SET line = '11' WHERE "coordinates" IN ('48.865381176,2.35564493121','48.871945798,2.37675104137');
 UPDATE ratp_stations SET line = '12' WHERE "coordinates" IN ('','');
-UPDATE ratp_stations SET line = '13' WHERE "coordinates" IN ('','');
-UPDATE ratp_stations SET line = '14' WHERE "coordinates" IN ('','');
+-- Champs-Élysées Clémenceau
+UPDATE ratp_stations SET line = '13' WHERE "coordinates" IN ('48.8677440261,2.31412278037','');
+-- Bercy,
+UPDATE ratp_stations SET line = '14' WHERE "coordinates" IN ('48.8401922444,2.37947725727','');
 UPDATE ratp_stations SET line = 'A' WHERE "name" IN ('','');
 UPDATE ratp_stations SET line = 'B' WHERE "name" IN ('','');
 
